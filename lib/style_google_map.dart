180b27176dff86105072203f0a896441947fa730
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2024-03-31 22:11:56 +0500

    map styles screen done

--- lib/style_google_map.dart
+++ lib/style_google_map.dart
@@ -11,24 +11,84 @@ class StyleGoogleMap extends StatefulWidget {
 }
 
 class _StyleGoogleMapState extends State<StyleGoogleMap> {
+  String mapTheme = '';
   final Completer<GoogleMapController> _controller = Completer();
   static const CameraPosition _kGooglePlex =
       CameraPosition(target: LatLng(24.798539, 67.135653), zoom: 14);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    DefaultAssetBundle.of(context)
+        .loadString('assets/theme/retro_theme.json')
+        .then((value) {
+      mapTheme = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Style Google Map',
+          style: TextStyle(color: Colors.white),
         ),
+        actions: [
+          PopupMenuButton(
+              iconColor: Colors.white,
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      onTap: () {
+                        _controller.future.then((value) {
+                          DefaultAssetBundle.of(context)
+                              .loadString('assets/theme/silver_theme.json')
+                              .then((string) {
+                            // ignore: deprecated_member_use
+                            value.setMapStyle(string);
+                          });
+                        });
+                      },
+                      child: const Text('Silver'),
+                    ),
+                    PopupMenuItem(
+                      onTap: () {
+                        _controller.future.then((value) {
+                          DefaultAssetBundle.of(context)
+                              .loadString('assets/theme/retro_theme.json')
+                              .then((string) {
+                            // ignore: deprecated_member_use
+                            value.setMapStyle(string);
+                          });
+                        });
+                      },
+                      child: const Text('Retro'),
+                    ),
+                    PopupMenuItem(
+                      onTap: () {
+                        _controller.future.then((value) {
+                          DefaultAssetBundle.of(context)
+                              .loadString('assets/theme/night_theme.json')
+                              .then((string) {
+                            // ignore: deprecated_member_use
+                            value.setMapStyle(string);
+                          });
+                        });
+                      },
+                      child: const Text('Night'),
+                    )
+                  ])
+        ],
         backgroundColor: Colors.blue,
       ),
       body: GoogleMap(
         initialCameraPosition: _kGooglePlex,
-        mapType: MapType.normal,
         myLocationButtonEnabled: true,
         myLocationEnabled: true,
         onMapCreated: (GoogleMapController controller) {
+          // ignore: deprecated_member_use
+          controller.setMapStyle(mapTheme);
           _controller.complete(controller);
         },
       ),
